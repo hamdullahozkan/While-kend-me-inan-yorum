2ac58409ff98806301de344ee6e64095539ad9bf
hamdullahozkan <hamdullahozkann@gmail.com>
2024-12-25 18:56:02 +0300

    Proje dosyası ekle.

---
+++ pratikkk/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i = 1; // Sayacımızı başlatıyoruz
+        while (i <= 10) // Koşul: i, 10'dan küçük veya eşit olduğu sürece
+        {
+            Console.WriteLine("Kendime inanıyorum, ben bu yazılım işini hallederim!");
+            i++; // Sayacı artırıyoruz
+        }
+    }
+}
